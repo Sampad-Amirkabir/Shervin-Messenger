d448b8f6a9a897c2bce5b23e3f63e1ad52fd4cbc
Mohammad Mostafa <ezmmd83@gmail.com>
2024-11-28 11:19:17 +0330

    updated to session 6

--- app/src/main/java/ir/shervin/messenger/pages/ChatPage.kt
+++ app/src/main/java/ir/shervin/messenger/pages/ChatPage.kt
@@ -1,19 +1,28 @@
 package ir.shervin.messenger.pages
 
 import android.net.Uri
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.rememberTransition
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -28,6 +37,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.Message
 import androidx.compose.material.icons.automirrored.rounded.Send
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.ClosedCaption
@@ -35,15 +45,18 @@ import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Send
 import androidx.compose.material.icons.rounded.TagFaces
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -52,17 +65,22 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
 import ir.shervin.messenger.R
+import ir.shervin.messenger.applyIf
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -81,6 +99,7 @@ fun ChatPage(navController: NavHostController) {
 	}
 	var fullscreen by remember { mutableStateOf(false) }
 	var viewImage by remember { mutableStateOf(null as Uri?) }
+	var isExpanded by remember { mutableStateOf(false) }
 	
 	Scaffold(
 		topBar = {
@@ -89,15 +108,32 @@ fun ChatPage(navController: NavHostController) {
 					Row(
 						verticalAlignment = Alignment.CenterVertically,
 						horizontalArrangement = Arrangement.SpaceBetween,
-						modifier = Modifier.fillMaxWidth()
+						modifier = Modifier
+							.fillMaxWidth()
+							.applyIf(!isExpanded) {
+								clickable {
+									isExpanded = true
+								}
+							}
 					) {
 						Text("Online", color = Color(0xFFE68415))
-						Image(painterResource(R.drawable.sohrab), null, modifier = Modifier.clip(CircleShape))
+						Image(
+							painterResource(R.drawable.sohrab),
+							null,
+							modifier = Modifier
+								.clip(CircleShape)
+						)
 						Text("Sohrab")
 					}
 				},
 				navigationIcon = {
-					IconButton(onClick = { navController.popBackStack() }) {
+					IconButton(onClick = {
+						if (isExpanded) {
+							isExpanded = false
+						} else {
+							navController.popBackStack()
+						}
+					}) {
 						Icon(Icons.AutoMirrored.Rounded.ArrowBack, null)
 					}
 				},
@@ -209,6 +245,98 @@ fun ChatPage(navController: NavHostController) {
 				) {
 					Icon(Icons.Rounded.Close, null)
 				}
+			}
+		}
+		
+		if (isExpanded) Popup {
+			var x by remember { mutableStateOf(false) }
+			val state = remember { MutableTransitionState(x) }
+			val transition = rememberTransition(state)
+			val imageSize by transition.animateDp { if (it) 400.dp else 66.dp }
+			val fillet by transition.animateDp { if (it) 0.dp else 200.dp }
+			val offset by transition.animateDp { if (it) 0.dp else (-3).dp }
+			val offset2 by transition.animateDp { if (it) 0.dp else 500.dp }
+			val scale by transition.animateFloat { if (it) 1f else 0f }
+			var isShrinking by remember { mutableStateOf(false) }
+			
+			BackHandler {
+				state.targetState = false
+				isShrinking = true
+			}
+			
+			BoxWithConstraints(
+				modifier = Modifier
+					.fillMaxSize()
+			) {
+				Image(
+					painterResource(R.drawable.sohrab),
+					null,
+					modifier = Modifier
+						.offset(offset)
+						.clip(RoundedCornerShape(fillet))
+						.align(Alignment.TopCenter)
+						.size(imageSize)
+				)
+				
+				Column(
+					modifier = Modifier
+						.offset(y = offset2)
+						.background(MaterialTheme.colorScheme.surface)
+						.fillMaxWidth()
+						.height(maxHeight - 400.dp)
+						.align(Alignment.BottomCenter)
+						.padding(20.dp)
+				) {
+					Text("Sohrab", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+					Text("Name", fontSize = 14.sp, color = Color(0xFFCCCCCC), fontWeight = FontWeight.Light)
+					
+					Spacer(Modifier.height(10.dp))
+					
+					Text("پو پو پو", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+					Text("Bio", fontSize = 14.sp, color = Color(0xFFCCCCCC), fontWeight = FontWeight.Light)
+					
+					Spacer(Modifier.height(10.dp))
+					
+					Text("+98 992 828 3720", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+					Text("Number", fontSize = 14.sp, color = Color(0xFFCCCCCC), fontWeight = FontWeight.Light)
+					
+					Spacer(Modifier.height(10.dp))
+					
+					Text("@AghrabePashmaloo", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+					Text("Username", fontSize = 14.sp, color = Color(0xFFCCCCCC), fontWeight = FontWeight.Light)
+					
+					Spacer(Modifier.height(10.dp))
+					
+					Row(
+						verticalAlignment = Alignment.CenterVertically,
+						horizontalArrangement = Arrangement.SpaceBetween,
+						modifier = Modifier.fillMaxWidth()
+					) {
+						Text("Notifications", fontSize = 20.sp)
+						Switch(true, null)
+					}
+				}
+				
+				FilledIconButton(
+					onClick = {},
+					modifier = Modifier
+						.offset(330.dp, 375.dp)
+						.scale(scale)
+						.clip(CircleShape)
+						.background(MaterialTheme.colorScheme.primary)
+						.padding(4.dp)
+				) {
+					Icon(Icons.AutoMirrored.Rounded.Message, null)
+				}
+			}
+			
+			LaunchedEffect(state.currentState) {
+				if (isShrinking && state.currentState.not())
+					isExpanded = false
+			}
+			
+			LaunchedEffect(Unit) {
+				state.targetState = true
 			}
 		}
 	}
